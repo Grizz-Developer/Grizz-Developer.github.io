a1836d3df1e9ddf71ec60409b7fbe6c0b1196766
Grizz-Developer <1843600489@qq.com>
2025-02-07 00:18:09 +0800

    Site updated: 2025-02-07 00:18:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/10/hello-world/","2024/05/16/如何用Hexo搭建个人博客/","2024/05/20/2分钟解决Android Studio下载gradle卡顿/"];function toRandomPost(){
+var posts=["2024/05/10/hello-world/","2024/05/16/如何用Hexo搭建个人博客/","2024/05/20/2分钟解决Android Studio下载gradle卡顿/","2024/06/07/Android Studio配置Gradle国内阿里开源镜像站/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
