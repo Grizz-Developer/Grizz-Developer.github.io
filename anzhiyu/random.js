7b9db56e1ca72fabbd28a98beefe5da1843f75e7
Hongyanng <1843600489@qq.com>
2025-02-06 22:59:50 +0800

    Site updated: 2025-02-06 22:59:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/11/hello-world/","2025/02/06/如何用Hexo搭建个人博客/"];function toRandomPost(){
+var posts=["2024/05/11/hello-world/","2024/02/06/如何用Hexo搭建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
