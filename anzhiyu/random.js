e26c74476bd8c2e97da10615a8ef971d024de90f
Grizz-Developer <1843600489@qq.com>
2025-04-22 18:30:53 +0800

    Site updated: 2025-04-22 18:30:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/10/hello-world/","2024/05/16/如何用Hexo搭建个人博客/","2024/05/20/2分钟解决Android Studio下载gradle卡顿/","2024/06/07/Android Studio配置Gradle国内阿里开源镜像站/","2025/02/16/Android四大组件/","2025/02/16/如何理解MVC、MVP、MVVM？他们的区别是什么？/","2025/02/17/Q&A/"];function toRandomPost(){
+var posts=["2024/05/10/hello-world/","2024/05/16/如何用Hexo搭建个人博客/","2024/05/20/2分钟解决Android Studio下载gradle卡顿/","2024/06/07/Android Studio配置Gradle国内阿里开源镜像站/","2025/02/16/Android四大组件/","2025/02/16/如何理解MVC、MVP、MVVM？他们的区别是什么？/","2025/02/17/Q&A/","2025/02/21/job/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
